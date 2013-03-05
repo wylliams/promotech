4b56f4bde17b272f498bba8fc676a2bc7ab0cbe9
hangrel@gmail.com <hangrel@gmail.com@d3f37603-1fc9-b202-c548-101fbb83d1c1>
2013-03-05 01:54:33 +0000

    classes de teste

---
+++ trunk/aplicacao/app/Test/Case/Model/TestPromotor.php
@@ -0,0 +1,37 @@
+public $fixtures = array('app.Promotor');
+public function startTest) {
+   $this->Promotor = ClassRegistry::init('Promotor');
+}
+public function testNoPassedData() {
+   $data = Array()
+   $this->assertFalse($this->Promotor->save($data));
+   $error_fields = array_keys($this->Promotor->validationErrors);
+   // Verifica se o campo deu erro na validação
+   $this->assertTrue(in_array('name', $error_fields);
+   $this->assertTrue(in_array('foto', $error_fields);
+   $this->assertTrue(in_array('categoria', $error_fields);
+   $this->assertTrue(in_array('altura', $error_fields);
+   $this->assertTrue(in_array('manequim', $error_fields);
+   $this->assertTrue(in_array('peso', $error_fields);
+   $this->assertTrue(in_array('telefone_celular', $error_fields);
+   $this->assertTrue(in_array('email', $error_fields);
+}
+public function testEmptyData() {
+   $data = Array(
+      'nome' => '',
+      'foto' => '',
+      'categoria' => ''
+	  'altura' => ''
+	  'manequim' => ''
+	  'email' => ''
+	  'peso' => ''
+	  'numero_calcado' => ''
+	  'telefone_celular' => ''
+   )
+   $this->assertFalse($this->Promotor->save($data));
+   $error_fields = array_keys($this->Promotor->validationErrors);
+   // Verifica se o campo deu erro na validação
+   $this->assertTrue(in_array('name', $error_fields);
+   $this->assertTrue(in_array('description', $error_fields);
+   $this->assertTrue(in_array('price', $error_fields);
+}
