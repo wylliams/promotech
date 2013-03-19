5e59ba8df4f73d32b733048a98c27e84cfb02289
prisaraujo92@gmail.com <prisaraujo92@gmail.com@d3f37603-1fc9-b202-c548-101fbb83d1c1>
2013-03-19 11:51:42 +0000

    [Prototipo]. Inicio da construcao tela de login.

---
+++ prototipo/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="charset=UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="section">
+			<div class="logo"></div>
+			<div class="info">
+				<div class="inputs">
+					<label>
+						Usuário: 
+						<input type="text" id="user" name="usuario" value="">
+					</label>
+				</div>
+				<br>
+				<div class="inputs">
+					<label>
+						Senha:
+						<input type="text" name="senha" value="">
+					</label>
+				</div>
+				<a href="">Esqueceu sua senha?</a>
+			</div>
+		</div>
+	</body>
+</html>
+
