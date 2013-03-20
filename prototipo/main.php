aaabac0b6fb580d43d67a087eb7e9123e93088c6
prisaraujo92@gmail.com <prisaraujo92@gmail.com@d3f37603-1fc9-b202-c548-101fbb83d1c1>
2013-03-20 13:06:27 +0000

    [Prototipo]Criacao da tela main

---
+++ prototipo/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="charset=UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="topo">
+			<img src="./img/logo-main.png">
+			<div class="user">
+				<img src="./img/user.jpg">
+				<div class="user-p">
+					<p>Bem Vindo, Fulaninha!</p>
+					<p>Cargo: Promotor</p>
+					<p>Último acesso: 19/03/2013 10:03</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
