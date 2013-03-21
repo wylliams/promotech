c58de40902984fe4dc45581319ff4add2dee07b4
prisaraujo92@gmail.com <prisaraujo92@gmail.com@d3f37603-1fc9-b202-c548-101fbb83d1c1>
2013-03-21 14:23:06 +0000

    [Prototipo]Tela main.php com topo finalizado e dados da pagina de campanha

--- prototipo/main.php
+++ prototipo/main.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="charset=UTF-8"/>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="http://ajax.googleapis. com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 	</head>
 	<body>
 		<div class="topo">
@@ -15,6 +16,36 @@
 					<p>Último acesso: 19/03/2013 10:03</p>
 				</div>
 			</div>
+		</div>
+		<div class="menu">
+			<hr>
+			<ul class="dropdown">
+				<li><a href="#"><img src="./img/btn_home.png"></a></li>
+				<li><a href="#">Agenda</a></li>
+				<li><a href="#">Clientes</a></li>
+				<li><a href="#">Promotor</a></li>
+				<li><a href="#">Coordenador</a></li>
+				<li><a href="#">Fornecedor</a></li>
+				<li><a href="#" class="ativo">Campanha</a></li>
+				<li><a href="#">Relatórios</a></li>
+			</ul> <!--fim ul dropdown-->
+		</div>
+		<div class="conteudo">
+			<div class="left_side">
+				<h2>Ações</h2>
+				<br>
+				<div class="btn" type="submit" id="btn-acoes">Nova Campanha</div>
+				<br>
+				<div class="btn" type="submit" id="btn-acoes">Listar Promotores</div>
+				<br>
+				<div class="btn" type="submit" id="btn-acoes">Listar Clientes</div>
+			</div>
+			<div class="right_side">
+				<h2>Campanha</h2>
+			</div>
+		</div>
+		<div class="rodape">
+			<p>2013 PROMOTECH - TODOS OS DIREITOS RESERVADOS</p>
 		</div>
 	</body>
 </html>
